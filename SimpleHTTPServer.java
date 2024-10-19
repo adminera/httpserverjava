24bb58c5f698590c2b75f1f583f2d9fe7a632d78
adminera <burntcharcoal@proton.me>
2024-10-19 15:54:35 +0100

    Update SimpleHTTPServer.java

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -4,6 +4,7 @@ import java.net.Socket; // allows for a client to connect to the server
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 
 public class SimpleHTTPServer {
@@ -16,7 +17,21 @@ public class SimpleHTTPServer {
         while (true) {
             // this will allow for a client to connect as long as the server is running which is (true)
             final Socket client = server.accept();
-            
+            // this creates an object that will get the input stream of the client connecting to the server
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            // this will read the input stream and convert it into a readable format
+            BufferedReader reader = new BufferedReader(isr);
+            // this will store the content that is read from the stream into a line to be printed
+            String line = reader.readLine();
+            // while line is not empty, it will print it out
+            while (!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+
+
+
+
 
         }
 
